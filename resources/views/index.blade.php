b8bd499ca078621905fcd9727bdc0390876c5cc6
Agustin Zamar <agustinzamar33@gmail.com>
2020-01-21 21:08:35 -0300

    Layout e index listos en blade

---
+++ resources/views/index.blade.php
@@ -0,0 +1,120 @@
+@extends('layouts.app')
+
+@section('styles')
+    <link href="{{ asset('css/index.css') }}" rel="stylesheet" />
+@endsection
+
+@section('title', 'Inicio')
+
+@section('main')
+<div class="portada">
+		
+		<h1> #ConstruyendoCiudadania </h1>
+
+	</div>
+
+	<section class="section1">
+		
+		<p>EN LA <span>FÁBRICA</span> DISEÑAMOS, DESARROLLAMOS E IMPLEMENTAMOS ESTRATEGIAS DE PARTICIPACIÓN CIUDADANA PARA CONTRIBUIR AL DESARROLLO DE LA SOCIEDAD CIVIL.</p>
+
+	</section>
+
+	<section class="section2">
+		
+		<p>¿Cómo trabajamos?</p>
+
+		<div class="trabajos">
+			
+			<div class="fortalecer item">
+				
+				<img src="{{ asset('img/fortalecer.png') }}" alt="Fortalecer">
+				<h2>Fortalecer</h2>
+				<p>Desarrollamos conocimientos y herramientas para consolidar el compromiso y mejorar las capacidades de las Organizaciones de la Sociedad Civil</p>
+
+			</div>
+
+			<div class="intervenir item">
+				
+				<img src="{{ asset('img/intervenir.png') }}" alt="Intervenir">
+				<h2>Intervenir</h2>
+				<p>Generamos acciones de participación ytrabajo colaborativo con la comunidad.</p>
+
+			</div>
+
+			<div class="compartir item">
+				
+				<img src="{{ asset('img/compartir.png') }}" alt="Compartir">
+				<h2>Compartir</h2>
+				<p>Brindamos información sobre derechos ciudadanos.</p>
+
+			</div>
+
+		</div>
+
+	</section>
+
+	<section class="section3">
+		
+		<p>¿Qué hacemos?</p>
+
+		<div class="cultivar item">
+			
+			<img src="{{ asset('img/cultivar.jpg') }}" alt="DefinirAlternativo">
+
+			<div class="texto">
+				
+				<h2>Cultivar</h2>
+				<p>Espacio de formación e investigación en el campo de las organizaciones sociales, la responsabilidad social y la sociedad civil.</p>
+				<a href="#">Saber mas</a>
+
+			</div>
+
+		</div>
+
+		<div class="jujuyLab item">
+			
+			<div class="texto right">
+				
+				<h2>JujuyLab</h2>
+				<p> Interfaz de participación ciudadana y trabajo colaborativo.</p>
+				<a href="#">Saber mas</a>
+			
+			</div>
+
+			<img src="{{ asset('img/lab.jpg') }}" alt="DefinirAlternativo">
+
+		</div>
+
+		<div class="normas item">
+			
+			<img src="{{ asset('img/normas.jpg') }}" alt="DefinirAlternativo">
+
+			<div class="texto">
+				
+				<h2>Las Normas que nos Normal</h2>
+				<p>Plataforma para difundir y promover los derechos ciudadanos.</p>
+				<a href="#">Saber mas</a>
+
+			</div>
+
+		</div>
+
+	</section>
+
+	<section class="section4">
+			
+		<div class="contacto">
+
+			<p>Formulario de Contacto</p>
+
+			<div class="formulario">
+			
+				<input type="text" placeholder="Tu correo">
+				<textarea placeholder="Escribe algo positivo..."></textarea>
+			
+			</div>
+		
+		</div>
+
+	</section>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,80 +1,72 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
+   
+    <title>{{ config('app.name', 'La Fabrica') }} - @yield('title')</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    @yield('scripts')
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/fc69885e8b.js" crossorigin="anonymous"></script>
+    @yield('fonts')
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/layout.css') }}" rel="stylesheet">
+    @yield('styles')
+
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+	
+	<header>
+		
+		<a href="#" class="logo"><img src="{{ asset('img/logo_gris.png') }}" alt="Logo La Fabrica"></a>
 
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
+		<div class="enlaces">
+			
+			<a href="#">La Fabrica</a>
+			<a href="#">¿Que hacemos?</a>
+			<a href="#">Novedades</a>
+			<a href="#">Contacto</a>
+			<a href="#">Inicio</a>
 
-                    </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
+			<div class="redes">
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+				<a href="#"><i class="fab fa-facebook-f"></i></a>
+				<a href="#"><i class="fab fa-twitter"></i></a>
+				<a href="#"><i class="fab fa-instagram"></i></a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
+			</div>
 
-        <main class="py-4">
-            @yield('content')
-        </main>
-    </div>
+		</div>
+
+	</header>
+
+    @section('main')
+    @show
+
+	<footer>
+		
+		<p>TODOS LOS DERECHOS RESERVADOS &copy; {{ \Carbon\Carbon::now()->format('Y') }} LA FABRICA</p>
+
+		<div class="redes">
+			
+			<a href="#"><i class="fab fa-facebook-f"></i></a>
+			<a href="#"><i class="fab fa-twitter"></i></a>
+			<a href="#"><i class="fab fa-instagram"></i></a>
+
+		</div>
+
+	</footer>
+
 </body>
 </html>
