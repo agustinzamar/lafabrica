fc5845ec3af170d53a75197c2b7c4f8324b0c2db
Franco Paez <franco.p.0109@gmail.com>
2020-01-31 02:49:18 -0300

    dejo planteado dos conceptos para las pestanias Normas y LabJujuy

--- resources/views/cultivar.blade.php
+++ resources/views/cultivar.blade.php
@@ -1,1 +1,31 @@
+@extends('layouts.main')
 
+@section('styles')
+    <link href="{{ asset('css/contenido.css') }}" rel="stylesheet" />
+@endsection
+
+@section('title', 'Inicio')
+
+@section('main')
+
+<div class="contenedor">
+	
+	<div class="contenedor__item">
+
+		<h1 class="item__titulo">CULTIVAR</h1>
+
+		<p class="item__parrafo">Cultivar es un programa de formación e investigación. Buscamos a través de él, contribuir al desarrollo de las organizaciones sociales, la responsabilidad social y la sociedad civil mediante herramientas, actividades y acciones que permitan aumentar su compromiso, capacidades, sustentabilidad e impacto social.
+		<br>
+		Para ello proponemos desplegar una serie de propuestas educativas, asesoramiento y consultorías para la orientacion y difusion de conocimientos relevantes.</p>
+
+	</div>
+
+	<div class="contenedor__item">
+		
+		<i class="material-icons" style="color: green;">spa</i>
+
+	</div>
+
+</div>
+
+@endsection
---
+++ resources/views/normas.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.main')
+
+@section('styles')
+    <link href="{{ asset('css/contenido.css') }}" rel="stylesheet" />
+@endsection
+
+@section('title', 'Inicio')
+
+@section('main')
+
+<div class="contenedorr">
+	
+		<h1 class="contenedor__titulo">LAS NORMAS QUE NOS NORMAN</h1>
+
+		<p class="contenedor__parrafo">Las Normas Que Nos Norman es una plataforma de información virtual que busca promover y difundir los marcos legales para situaciones cotidianas. Es una forma de  aprehender colectivamente sobre el ejercicio y exigibilidad de derechos y responsabilidades frente al Estado y las empresas.</p>
+
+</div>
+
+@endsection
